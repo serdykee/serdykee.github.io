0ff61027b848655b62544bfa4bbb92c5d63087f7
SerdyKee <serdykee@gmail.com>
2023-08-17 22:21:49 +0700

    up cert

--- js/filelist.js
+++ js/filelist.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://api.github.com/repos/serdykee/serdykee.github.io/contents/cert",
-        dataType: "json",
-        success: function(data) {
-            $.each(data, function(index, file) {
-                var fileUrl = file.download_url;
-                var fileName = file.name;
-                var listItem = "<li><a href='" + fileUrl + "' download>" + fileName + "</a></li>";
-                $("#file-list").append(listItem);
+$(document).ready(function() { 
+    $.ajax({ 
+        url: "https://api.github.com/repos/serdykee/serdykee.github.io/contents/cert", 
+        dataType: "json", 
+        success: function(data) { 
+            $.each(data, function(index, file) { 
+                var fileUrl = file.download_url; 
+                var fileName = file.name; 
+                var listItem = "<li><a href='" + fileUrl + "' download>" + fileName + "</a></li>"; 
+                $("#file-list").append(listItem); 
             });
-        }
-    });
+
+            $("#file-list").hide().fadeIn(1000);
+        } 
+    }); 
 });

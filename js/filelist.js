b15401bc0d2a032cccf1e97acb305132756aece4
SerdyKee <serdykee@gmail.com>
2023-08-17 22:54:34 +0700

    up

--- js/filelist.js
+++ js/filelist.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $.ajax({ 
         url: "https://api.github.com/repos/serdykee/serdykee.github.io/contents/cert", 
         dataType: "json", 
-        success: function(data) { 
+        success: function(data) {
+            var reversedData = data.reverse(); // Sắp xếp danh sách theo thứ tự đảo ngược
             $.each(data, function(index, file) { 
                 var fileUrl = file.download_url; 
                 var fileName = file.name; 

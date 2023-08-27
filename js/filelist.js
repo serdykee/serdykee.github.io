9c0c656f6e7d95bdaff50642bab4c971f820f5a3
SerdyKee <54352579+serdykee@users.noreply.github.com>
2023-08-27 17:50:20 +0700

    Update filelist.js

--- js/filelist.js
+++ js/filelist.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
         dataType: "json", 
         success: function(data) {
             var reversedData = data.reverse(); // Sắp xếp danh sách theo thứ tự đảo ngược
-            $.each(data, function(index, file) { 
+            $.each(reversedData, function(index, file) { 
                 var fileUrl = file.download_url; 
                 var fileName = file.name; 
-                var listItem = "<li><a href='" + fileUrl + "' download>" + fileName + "</a></li>"; 
+
+                // Lấy số thứ tự từ index và thêm số 0 vào đầu
+                var fileNumber = (index + 1).toString().padStart(2, '0');
+
+                var listItem = "<li><a href='" + fileUrl + "' download>" + "File " + fileNumber + ": " + fileName + "</a></li>"; 
                 $("#file-list").append(listItem); 
             });
 
